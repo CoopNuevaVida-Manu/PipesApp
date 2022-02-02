4b9058ae4ac546b16613f552e403373eb8035527
CoopNuevaVida-Manu <mvilleda@coopnuevavida.com>
2022-02-02 11:19:31 -0600

    Fin de la seecion 10

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 
 @Component({
@@ -9,7 +10,68 @@ import { Component } from '@angular/core';
 })
 export class NoComunesComponent {
 
-  nombre: string = 'Manuel';
+  //i18nSelect
+  nombre: string = 'Sara';
   genero: string = 'femenino'
+
+  invitacionMapa = {
+    'masculino': 'invitarlo',
+    'femenino': 'invitarla'
+  }
+
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Pedros', 'Juan']
+  clientesMapa = {
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    'other': `tenemos # clientes esperando`
+  }
+
+
+  cambiarCliente(){
+    if(this.nombre=== 'Sara'){
+      this.nombre = 'Manuel'
+      this.genero = 'masculino'
+    }else{
+      this.nombre = 'Sara'
+      this.genero = 'femenino'
+    }
+  }
+
+  borrarCliente(){
+    this.clientes.shift();
+  }
+
+  //key value
+  persona = {
+    nombre: 'Manuel',
+    edad: 23,
+    direccion: 'Tegucigalpa'
+  }
+
+  //JsonPipe
+  heroes= [
+    {
+      nombre: 'Superman',
+      vuela: true
+    },
+    {
+      nombre: 'Robin',
+      vuela: false
+    },
+    {
+      nombre: 'Acuaman',
+      vuela: false
+    }
+  ]
+
+  //Async Pipe
+  miObservable = interval(1000);
+  
+  valorPromesa = new Promise((resolve, reject) =>{
+    setTimeout(()=>{
+      resolve('Fin de la promesa')
+    }, 3500);
+  });
 
 }
